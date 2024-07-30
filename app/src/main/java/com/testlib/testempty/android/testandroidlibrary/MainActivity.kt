4816d7a699888cb4fee57f821a770b85cefccbfd
Vlad Legkiy <103931385+Vlad21anon@users.noreply.github.com>
2024-07-30 18:34:41 +0300

    Add files via upload

---
+++ app/src/main/java/com/testlib/testempty/android/testandroidlibrary/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.testlib.testempty.android.testandroidlibrary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.testlib.testempty.android.hellolib.HelloPrinter
+import com.testlib.testempty.android.testandroidlibrary.ui.theme.TestAndroidLibraryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TestAndroidLibraryTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    HelloPrinter().printTag("TAGGED")
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TestAndroidLibraryTheme {
+        Greeting("Android")
+    }
+}
